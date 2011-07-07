1b3147032b97018d86af565c557cf1e3199db37a
mturnwall <michael.turnwall@stlondemand.com>
2011-07-06 17:43:35 -0700

    updated to display days correctly and added ability to only display minutes and seconds countdown. Documentation added

--- countdown.js
+++ countdown.js
@@ -1,12 +1,12 @@
 /**
 *   @class Countdown Object
-*   @version 1.0
+*   @version 1.2
 *   @description handles counting down from today's date to a date in future
 *   @requires jQuery v1.3 or later
 *   @author Michael Turnwall
 *   
-*   Copyright (c) 2011 Michael Turnwall
-*   Released under a GPLv3 license.
+*   <p>Copyright (c) 2011 Michael Turnwall</p>
+*   <p>Released under a GPLv3 license.</p>
 *   <p>This program is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
 *   the Free Software Foundation, either version 3 of the License, or
@@ -20,45 +20,32 @@
 *   <p>You should have received a copy of the GNU General Public License
 *   along with this program.  If not, see <http://www.gnu.org/licenses/>.</p>
 */
-var Countdown = /** @lends Countdown.prototype */ {
-    /** 
-	*   initializes the object
-	*   @param {Array} eventDate the date counting down to, [yyyy, m, d]
-    *       the month is zero based so January is 0, December is 11.
-	*   @constructs
-	*/
-    init: function (eventDate) {
-        var obj = this;
-        this.$_timeContainers = $('.timerCont .timeValue span'); // HTML elements that hold the time values
-        this.timeValues = [];
-        this.timer;     // setTimeout timer
-        this.pacificOffset = 420;
-        
-        this.getTimeDifference(eventDate);  // find the difference between event date and today's date
-        this.writeTime(this.timeValues);    // write the difference to the page and start countdown
-    },
-
+var Countdown = /** @lends Countdown.prototype */{
     /**
-    *   find the difference in days, hours, mins, seconds between two dates.
-    *   @param {Array} eventDate the date counting down to, [yyyy, m, d]
-    *   the month is zero based so January is 0, December is 11.
-    */
-    getTimeDifference: function (eventDate) {
+     *   find the difference in days, hours, mins, seconds between two dates.
+     *   @param {Array} eventDate the date counting down to, [yyyy, m, d]
+     *   the month is zero based so January is 0, December is 11.
+     */
+    getTimeDifference: function(eventDate) {
         var difference,
-            endDate,
-            oneDay,
-            oneHour,
-            oneMinute,
-            oneSecond,
-            startDate,
-            values;
-        
+                endDate,
+                oneDay,
+                oneHour,
+                oneMinute,
+                oneSecond,
+                startDate,
+                values;
+
         values = [];
         oneDay = 1000 * 60 * 60 * 24;
         oneHour = 1000 * 60 * 60;
         oneMinute = 1000 * 60;
         oneSecond = 1000;
-        endDate = new Date(eventDate[0], eventDate[1], eventDate[2]);
+        if (eventDate.length < 4) {
+            endDate = new Date(eventDate[0], eventDate[1], eventDate[2]);
+        } else {
+            endDate = new Date(eventDate[0], eventDate[1], eventDate[2], eventDate[3], eventDate[4]);
+        }        
         startDate = new Date();
         
         /** subtract the event date with today's date minus the timezone difference */
@@ -66,27 +53,43 @@ var Countdown = /** @lends Countdown.prototype */ {
 
         // Thanks to Richard Rudzinski for reminding me about modulus!
         // days
-        this.timeValues.push(Math.ceil(difference / oneDay));
+        if (difference >= oneDay) {
+            this.timeValues['days'] = Math.floor(difference / oneDay);
+        } else {
+            this.timeValues['days'] = 0;
+        }
         // hours
-        this.timeValues.push(Math.ceil((difference % oneDay) / oneHour));
+        this.timeValues['hours'] = Math.floor((difference % oneDay) / oneHour);
         // minutes
-        this.timeValues.push(Math.ceil(((difference % oneDay) % oneHour) / oneMinute));
+        this.timeValues['mins'] = Math.floor(((difference % oneDay) % oneHour) / oneMinute);
         // seconds
-        this.timeValues.push(Math.ceil((((difference % oneDay) % oneHour) % oneMinute) / oneSecond));
+        this.timeValues['seconds'] = Math.ceil((((difference % oneDay) % oneHour) % oneMinute) / oneSecond);
     },
-    
     /**
-    *   Find the current timezone the user's computer is in and then move it back to PDT
-    *   @return {Integer} the total difference between current timezone and PDT in milliseconds
-    */
-    findTimezone: function () {
+     *  handle just countdowning hours and minutes
+     */
+    startTimeCounter: function (eventDate) {
+        var extraMins;
+        this.getTimeDifference(eventDate);
+        if (this.timeValues['hours']) {
+            extraMins = this.timeValues['hours'] * 60;  // take any extra hours and add them to the total minutes
+            this.timeValues['mins'] = this.timeValues['mins'] + extraMins;
+        }
+    },
+
+    /**
+     *   Find the current timezone the user's computer is in and then move it back to PDT
+     *   @return {Integer} the total difference between current timezone and PDT in milliseconds
+     */
+    findTimezone: function() {
         var now, currentZone;
-        
+
         now = new Date();
         currentZone = now.getTimezoneOffset();
-        if (currentZone == 420) {
+        if (currentZone == this.pacificOffset) {
             return 0;
         }
+
         if (currentZone < 0) {
             currentZone = (currentZone * -1) + this.pacificOffset;
         } else if (currentZone < this.pacificOffset) {
@@ -94,20 +97,20 @@ var Countdown = /** @lends Countdown.prototype */ {
         } else {
             currentZone = this.pacificOffset + currentZone;
         }
-        
+
         return currentZone * 1000 * 60;
     },
-    
+
     /**
-    *   take current time value and deduct a second
-    */
-    countTime: function () {
+     *   take current time value and deduct a second
+     */
+    countTime: function() {
         // temp variables so we have a copy to manipulate
-        var days = this.timeValues[0],
-        hours = this.timeValues[1],
-        mins = this.timeValues[2],
-        secs = this.timeValues[3],
-        time;
+        var days = this.timeValues['days'],
+            hours = this.timeValues['hours'],
+            mins = this.timeValues['mins'],
+            secs = this.timeValues['seconds'],
+            time;
         if (secs > 0) {
             secs--;
         } else if (secs == 0 && mins > 0) {
@@ -127,30 +130,65 @@ var Countdown = /** @lends Countdown.prototype */ {
                 hours = 0;
             }
         }
-        time = [days, hours, mins, secs];
+        time = {
+            days: (days >= 0) ? days : 0,
+            hours: (hours >= 0) ? hours : 0,
+            mins: (mins >= 0) ? mins : 0,
+            seconds: (secs >= 0) ? secs : 0
+        };
         /** write the new time values back to the main time array for next time */
-        for (var i = time.length - 1; i >= 0; i--){
+        for (var i in time) {
             this.timeValues[i] = time[i];
         };
         this.writeTime(time);
     },
-    
+
     /**
-    *   write the new time values into the page
-    */
-    writeTime: function (values) {
+     *   write the new time values into the page
+     */
+    writeTime: function(values) {
         var obj = this;
-        this.$_timeContainers.each(function (i) {
+        this.$_timeContainers.each(function(i) {
+            var timeType = this.parentNode.className.match(/days|hours|mins|seconds/)[0];
             // add leading zero if needed
-            (values[i].toString().length > 1) ? this.innerHTML = values[i] : this.innerHTML = '0' + values[i];
+            if (values[timeType] >= 0) {
+                (values[timeType].toString().length > 1) ? this.innerHTML = values[timeType] : this.innerHTML = '0' + values[timeType];
+            }
         });
         // only continue countdown if all values aren't zero
-        if (values[0] !=0 || values[1] !=0 || values[2] !=0 || values[3] !=0) {
-            this.timer = window.setTimeout(function () {
+        if (values['days'] != 0 || values['hours'] != 0 || values['mins'] != 0 || values['seconds'] != 0) {
+            this.timer = window.setTimeout(function() {
                 obj.countTime();
             }, 1000);
         } else {
-            return 'Lets shuffle and deal!';
+            return window.location.reload();
         }
+    },
+    /** 
+     *   initializes the object
+     *   @param opts options for the object to initialize with
+     *   @param {Array} opts.eventDate the date counting down to, [yyyy, m, d, hh, mm, ss]
+     *       the month is zero based so January is 0, December is 11.
+     *   @param {Boolean} opts.levelTimer true if the timer is used for a level timer instead of
+             an upcoming event timer
+     *   @param {Boolean} opts.dst whether or not daylight savings is currently in affect
+     *   @param {String} opts.status a message to be displayed
+     */
+    init: function(opts) {
+        this.$_timeContainers = $('.timerCont .timeValue span'); // HTML elements that hold the time values
+        if (opts.eventStatus) {
+            $('.timerCont').html(opts.eventStatus);
+            return true;
+        }
+        this.timeValues = [];
+        this.timer;     // setTimeout timer
+        this.dst = (typeof opts.dst == 'undefined') ? false : opts.dst;
+        this.pacificOffset = (this.dst) ? 420 : 480;    // find out if we have PST or PDT
+        if (!opts.levelTimer) {
+            this.getTimeDifference(opts.eventDate);  // find the difference between event date and today's date
+        } else {
+            this.startTimeCounter(opts.eventDate);
+        }
+        this.writeTime(this.timeValues);    // write the difference to the page and start countdown
     }
 };
